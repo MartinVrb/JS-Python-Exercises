2f36b6857a15a1646de00c3248bafe9c9fb07de9
Martin Varbanov <148352793+MartinVrb@users.noreply.github.com>
2024-03-22 22:40:11 +0200

    Add files via upload

---
+++ 1. JS-Basics/1.First Steps in Coding/1. First Steps In Coding - Lab/04_Inches_to_Centimeters.js
@@ -0,0 +1,10 @@
+function inchesToCentimeter(input){
+
+    let inches = Number(input[0]);
+    let centimeters = inches * 2.54;
+
+    console.log(centimeters);
+
+}
+inchesToCentimeter(["5"]);
+inchesToCentimeter(["7"]);
