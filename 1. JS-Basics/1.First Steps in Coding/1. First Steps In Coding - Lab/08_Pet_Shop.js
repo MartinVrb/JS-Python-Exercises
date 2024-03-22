f1581cadeb42877ed9f698d0191ec745ea5f08e4
Martin Varbanov <148352793+MartinVrb@users.noreply.github.com>
2024-03-22 22:41:10 +0200

    Add files via upload

---
+++ 1. JS-Basics/1.First Steps in Coding/1. First Steps In Coding - Lab/08_Pet_Shop.js
@@ -0,0 +1,10 @@
+function petShop(input){
+
+    let numDogsFood = Number(input[0]);
+    let numCatsFood = Number(input[1]);
+    
+    
+    console.log(`${numDogsFood * 2.50 + numCatsFood * 4} lv. `);
+
+}
+petShop([ "13", "9"]);
