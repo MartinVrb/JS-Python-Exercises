48ba6a497be00fbbc839b6f9f7e8d6dd3a33aadc
Martin Varbanov <148352793+MartinVrb@users.noreply.github.com>
2024-03-25 23:21:33 +0200

    Add files via upload

---
+++ 1. JS-Basics/2. Conditional Statements/2. Conditional Statements - Exercise/06_World_Swimming_Record.js
@@ -0,0 +1,20 @@
+function worldSwimmingRecord(input){
+
+    let recordInSeconds = Number(input[0]);
+    let distanceInMeters = Number(input[1]);
+    let timeFor1Meter = Number(input[2]);
+
+
+    let totalTimeNeeded = distanceInMeters * timeFor1Meter;
+    let delay = Math.floor(distanceInMeters / 15) * 12.5;
+
+    let totalTimeWithDelay = totalTimeNeeded + delay;
+
+    if(totalTimeWithDelay < recordInSeconds){
+        console.log(`Yes, he succeeded! The new world record is ${totalTimeWithDelay.toFixed(2)} seconds.`);
+    } else{
+        console.log(`No, he failed! He was ${(totalTimeWithDelay - recordInSeconds).toFixed(2)} seconds slower.`);
+    }
+
+}
+worldSwimmingRecord([ "10464", "1500", "20"]);
