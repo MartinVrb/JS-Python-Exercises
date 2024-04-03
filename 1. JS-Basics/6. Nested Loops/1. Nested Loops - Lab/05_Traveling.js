704bea427bfb9ccc48e7ff34cf8f171c7887880c
Martin Varbanov <148352793+MartinVrb@users.noreply.github.com>
2024-04-03 23:27:55 +0300

    Add files via upload

---
+++ 1. JS-Basics/6. Nested Loops/1. Nested Loops - Lab/05_Traveling.js
@@ -0,0 +1,30 @@
+function traveling(input) {
+
+    let index = 0;
+    let command = input[index];
+    index++;
+
+
+    while (command !== "End") {
+
+        let destination = command;
+        let budget = Number(input[index]);
+        index++;
+
+        let saveMoney = 0;
+
+        while (saveMoney < budget) {
+            let money = Number(input[index]);
+            index++;
+            saveMoney += money;
+        }
+
+        console.log(`Going to ${destination}!`);
+
+        command = input[index];
+        index++;
+
+    }
+
+}
+traveling(["France", "2000", "300", "300", "200", "400", "190", "258", "360", "Portugal", "1450", "400", "400", "200", "300", "300", "Egypt", "1900", "1000", "280", "300", "500", "End"])
