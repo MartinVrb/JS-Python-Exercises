26d21c9699ebe4ffd2041a92963679ce7239d58d
Martin Varbanov <148352793+MartinVrb@users.noreply.github.com>
2024-04-06 23:48:05 +0300

    Add files via upload

---
+++ 2. JS Fundamentals/1. Basic Syntax, Cond. Statements and Loops/2. Basic Syntax, Cond. Statements and Loops - Exercise/07_Triangle_Of_Numbers.js
@@ -0,0 +1,11 @@
+function triangleOfNumbers(maxRow) {
+
+    for (let curRow = 1; curRow <= maxRow; curRow++) {
+        console.log(`${curRow} `.repeat(curRow));
+
+    }
+
+
+
+}
+triangleOfNumbers(3);
