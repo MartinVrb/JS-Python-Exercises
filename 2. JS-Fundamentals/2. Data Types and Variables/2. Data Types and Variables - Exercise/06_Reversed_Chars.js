89a083f829fd2e925c0ed50edf2b2fc75b5034a3
Martin Varbanov <148352793+MartinVrb@users.noreply.github.com>
2024-04-09 23:01:15 +0300

    Add files via upload

---
+++ 2. JS-Fundamentals/2. Data Types and Variables/2. Data Types and Variables - Exercise/06_Reversed_Chars.js
@@ -0,0 +1,12 @@
+function reversedChars(char1, char2, char3) {
+
+    let buff = ``;
+
+    buff += `${char3} `;
+    buff += `${char2} `;
+    buff += `${char1}`;
+
+    console.log(buff);
+
+}
+reversedChars('A', 'B', 'C');
