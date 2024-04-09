3ce40e710460b55456df5173d699d55936bc15c8
Martin Varbanov <148352793+MartinVrb@users.noreply.github.com>
2024-04-09 23:00:20 +0300

    Add files via upload

---
+++ 2. JS-Fundamentals/2. Data Types and Variables/2. Data Types and Variables - Exercise/02_Chars_to_String.js
@@ -0,0 +1,14 @@
+function charsToString(par1, par2, par3) {
+
+    let value = true;
+    let buff = ``;
+
+    buff += `${par1}`;
+    buff += `${par2}`;
+    buff += `${par3}`;
+
+    console.log(buff);
+
+
+}
+charsToString('%', '2', 'o');
