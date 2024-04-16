6eaf7b0ad2b41fcda5eb7bffed3d3e319729cc09
Martin Varbanov <148352793+MartinVrb@users.noreply.github.com>
2024-04-16 23:17:50 +0300

    Add files via upload

---
+++ 2. JS-Fundamentals/5. Arrays Advanced/1. Arrays Advanced - Lab/02_Negative_or_Positive_Numbers.js
@@ -0,0 +1,18 @@
+function negativeOrPositiveNumbers(array) {
+
+    let newArray = [];
+
+
+    for (let i = 0; i < array.length; i++) {
+
+        if (array[i] < 0) {
+            newArray.unshift(array[i]);
+        } else {
+            newArray.push(array[i]);
+        }
+    }
+
+    console.log(newArray.join('\n'));
+
+}
+negativeOrPositiveNumbers(['-5', '-4', '0', '-1']);
