e70d88b42b0eb4cf03a1634998fa01a0ad4a6c38
Martin Varbanov <148352793+MartinVrb@users.noreply.github.com>
2024-04-17 23:14:09 +0300

    Add files via upload

---
+++ 2. JS-Fundamentals/5. Arrays Advanced/1. Arrays Advanced - Lab/04_Last_Numbers_Sequance.js
@@ -0,0 +1,17 @@
+function lastNumbersSequence(n, k) {
+    let arr = [1];
+
+    while (n > arr.length) {
+        let kArr = arr.slice(-k);
+        let sum = 0;
+
+        for (let i = 0; i < kArr.length; i++) {
+            sum += kArr[i];
+        }
+
+        arr.push(sum);
+    }
+
+    console.log(arr.join(` `));
+}
+lastNumbersSequence(6, 3);
