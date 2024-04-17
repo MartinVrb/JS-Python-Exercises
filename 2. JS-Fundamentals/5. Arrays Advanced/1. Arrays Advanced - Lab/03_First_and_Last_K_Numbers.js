d78598eaf09ac8bc58e137d856d476ee6c02e11b
Martin Varbanov <148352793+MartinVrb@users.noreply.github.com>
2024-04-17 23:13:56 +0300

    Add files via upload

---
+++ 2. JS-Fundamentals/5. Arrays Advanced/1. Arrays Advanced - Lab/03_First_and_Last_K_Numbers.js
@@ -0,0 +1,11 @@
+function firstAndLastKNumbers(data) {
+
+    let k = data.shift();
+    let firstK = data.slice(0, k);
+    let lastK = data.slice(data.length - k);
+
+    console.log(firstK.join(" "));
+    console.log(lastK.join(" "));
+
+}
+firstAndLastKNumbers([3, 6, 7, 8, 9]);
