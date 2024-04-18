5a4d120ecc240b751259e95bdee29f5929d364b8
Martin Varbanov <148352793+MartinVrb@users.noreply.github.com>
2024-04-18 23:32:02 +0300

    Add files via upload

---
+++ 2. JS-Fundamentals/5. Arrays Advanced/2. Arrays Advanced - Exercise/02_Distinct_Array.js
@@ -0,0 +1,14 @@
+function distinctArray(arr) {
+
+    let newArray = [];
+
+    for (let num of arr) {
+
+        if (!newArray.includes(num)) {
+            newArray.push(num);
+        }
+    }
+
+    console.log(newArray.join(' '));
+}
+distinctArray([7, 8, 9, 7, 2, 3, 4, 1, 2]);
