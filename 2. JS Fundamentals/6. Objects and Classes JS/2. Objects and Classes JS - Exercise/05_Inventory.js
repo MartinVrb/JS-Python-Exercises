d87330824494475ce0328c6ff17e36a37efd9cdd
Martin Varbanov <148352793+MartinVrb@users.noreply.github.com>
2024-09-30 01:16:48 +0300

    Add files via upload

---
+++ 2. JS Fundamentals/6. Objects and Classes JS/2. Objects and Classes JS - Exercise/05_Inventory.js
@@ -0,0 +1,20 @@
+function inventory(arr) {
+    let heroes = [];
+
+    for (let heroInfo of arr) {
+        let [heroName, heroLevel, heroItems] = heroInfo.split(" / ");
+        heroLevel = Number(heroLevel);
+
+        let heroObj = { name: heroName, level: heroLevel, items: heroItems };
+        heroes.push(heroObj);
+    }
+
+    let sortedHeroes = heroes.sort((a, b) => a.level - b.level);
+
+    for (let heroObj of sortedHeroes) {
+        console.log(`Hero: ${heroObj.name}`);
+        console.log(`level => ${heroObj.level}`);
+        console.log(`items => ${heroObj.items}`);
+    }
+}
+inventory(['Isacc / 25 / Apple, GravityGun', 'Derek / 12 / BarrelVest, DestructionSword', 'Hes / 1 / Desolator, Sentinel, Antara']);
