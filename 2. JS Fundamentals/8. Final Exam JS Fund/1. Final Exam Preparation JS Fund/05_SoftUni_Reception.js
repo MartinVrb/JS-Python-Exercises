8dd4824860749b299272ab0b0a7a93286898ac63
Martin Varbanov <148352793+MartinVrb@users.noreply.github.com>
2024-10-05 00:08:29 +0300

    Add files via upload

---
+++ 2. JS Fundamentals/8. Final Exam JS Fund/1. Final Exam Preparation JS Fund/05_SoftUni_Reception.js
@@ -0,0 +1,24 @@
+function softUniReception(input) {
+
+    let employeeOne = Number(input.shift());
+    let employeeTwo = Number(input.shift());
+    let employeeThree = Number(input.shift());
+    let people = Number(input.shift());
+
+    let answersPerHour = employeeOne + employeeTwo + employeeThree;
+    let neededHours = 0;
+
+    while (people > 0) {
+
+        neededHours++;
+        people -= answersPerHour;
+
+        if (neededHours % 4 === 0) {
+            neededHours++;
+        }
+    }
+
+    console.log(`Time needed: ${neededHours}h.`);
+
+}
+softUniReception(['1', '2', '3', '45']);
